fd709f13b44fb5ce537e1e1138191ad0feb65c66
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-06 09:34:36 +0300

    feat: neurotctl arguments implementation

--- src/bin/neuroctl.rs
+++ src/bin/neuroctl.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, ValueEnum};
+use serde::Serialize;
+
+#[derive(Serialize, Clone, ValueEnum, Debug)]
+#[serde(rename_all = "kebab-case")]
+enum Command {
+    Get,
+    Put,
+    Del,
+}
+
+/// `neuroctl` is a command line client for `neuro`.
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, env, value_delimiter = ',')]
+    endpoints: Vec<String>,
+    command: Command,
+    key: String,
+    value: Option<String>,
+}
+
 pub fn main() {
-    println!("neuroctl --help");
+    let args = Args::parse();
+
+    println!("Cluster endpoints: {:?}", args.endpoints);
+    println!(
+        "neuroctl: {:?} {:?} {:?}",
+        args.command, args.key, args.value
+    );
 }
