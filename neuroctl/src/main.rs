e85fface53a32a381f736508b3a0738e3e147e02
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-07 21:36:23 +0300

    feat: neuroctl cli connection to tcp server

--- neuroctl/src/main.rs
+++ neuroctl/src/main.rs
@@ -1,5 +1,23 @@
+use std::{
+    io::{self, Read, Write},
+    net::TcpStream,
+};
+
 use clap::{Parser, ValueEnum};
+use neurod::{KvCommand, KvResponse};
 use serde::Serialize;
+
+#[derive(Debug, thiserror::Error)]
+enum CliError {
+    #[error("io error: {0}")]
+    Io(#[from] io::Error),
+    #[error("json error: {0}")]
+    Json(#[from] serde_json::Error),
+    #[error("no endpoints provided")]
+    NoEndpoints,
+    #[error("value required for put command")]
+    ValueRequired,
+}
 
 #[derive(Serialize, Clone, ValueEnum, Debug)]
 #[serde(rename_all = "kebab-case")]
@@ -20,12 +38,76 @@ struct Args {
     value: Option<String>,
 }
 
-pub fn main() {
+fn write_message(stream: &mut TcpStream, msg: &[u8]) -> io::Result<()> {
+    let len: u32 = msg
+        .len()
+        .try_into()
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "message too large"))?;
+    stream.write_all(&len.to_be_bytes())?;
+    stream.write_all(msg)?;
+    stream.flush()?;
+    Ok(())
+}
+
+fn read_message(stream: &mut TcpStream) -> io::Result<Vec<u8>> {
+    let mut len_buf = [0u8; 4];
+    stream.read_exact(&mut len_buf)?;
+    let len = u32::from_be_bytes(len_buf) as usize;
+
+    let mut msg_buf = vec![0u8; len];
+    stream.read_exact(&mut msg_buf)?;
+    Ok(msg_buf)
+}
+
+fn main() -> Result<(), CliError> {
     let args = Args::parse();
 
-    println!("Cluster endpoints: {:?}", args.endpoints);
-    println!(
-        "neuroctl: {:?} {:?} {:?}",
-        args.command, args.key, args.value
-    );
+    if args.endpoints.is_empty() {
+        return Err(CliError::NoEndpoints);
+    }
+
+    let mut stream = TcpStream::connect(&args.endpoints[0])?;
+
+    let command = match args.command {
+        Command::Get => KvCommand::Get { key: args.key },
+        Command::Put => {
+            let value = args.value.ok_or(CliError::ValueRequired)?;
+            KvCommand::Put {
+                key: args.key,
+                value,
+            }
+        }
+        Command::Del => KvCommand::Del { key: args.key },
+    };
+
+    let command_json = serde_json::to_vec(&command)?;
+    write_message(&mut stream, &command_json)?;
+    let response_bytes = read_message(&mut stream)?;
+    let response: KvResponse = serde_json::from_slice(&response_bytes)?;
+
+    match response {
+        KvResponse::Ok { value } => {
+            if let Some(v) = value {
+                println!("{v}");
+            } else {
+                println!("OK");
+            }
+        }
+        KvResponse::NotFound => {
+            eprintln!("Key not found");
+            std::process::exit(1);
+        }
+        KvResponse::NotLeader {
+            leader_addr: _,
+            members: _,
+        } => {
+            std::process::exit(1);
+        }
+        KvResponse::InvalidKey => {
+            eprintln!("Invalid key");
+            std::process::exit(1);
+        }
+    }
+
+    Ok(())
 }
