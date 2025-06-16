04287e3da9b4a91071ae194e6de2830a4e9145d2
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-16 22:22:56 +0300

    chore: todo fn

--- raft/src/lib.rs
+++ raft/src/lib.rs
@@ -6,7 +6,7 @@ pub mod log;
 #[derive(Debug, thiserror::Error)]
 pub enum RaftNodeError {
     #[error("log operation failed")]
-    LogError(#[from] LogError)
+    LogError(#[from] LogError),
 }
 
 pub trait StateMachine {
@@ -16,27 +16,19 @@ pub trait StateMachine {
     fn apply(&mut self, command: Self::Command) -> Self::Response;
 }
 
-
 #[derive(Default, Debug)]
 pub struct RaftNode<S: StateMachine> {
-    log: Log<S::Command>,
-    state_machine: S,
+    pub log: Log<S::Command>,
+    pub state_machine: S,
 }
 
 impl<S: StateMachine> RaftNode<S> {
-    // /// Applies all committed but not yet applied entries to the state machine
-    // ///
-    // /// # Errors
-    // ///
-    // /// Returns `RaftNodeError::CastError` if `at` fails
-    // pub fn apply_committed_entries(&mut self) -> Result<(), RaftNodeError> {
-    //     while self.log.applied < self.log.committed {
-    //         let entry = &self.log.at(self.log.applied)?;
-    //         self.state_machine.apply(entry.command.clone());
-    //         self.log.applied += 1;
-    //         // TODO: notify that we have applied the index
-    //     }
-    //
-    //     Ok(())
-    // }
+    /// Applies all committed but not yet applied entries to the state machine
+    ///
+    /// # Errors
+    ///
+    /// Returns `RaftNodeError::CastError` if `at` fails
+    pub fn apply_committed_entries(&mut self) -> Result<(), RaftNodeError> {
+        todo!()
+    }
 }
