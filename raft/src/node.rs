222b9c56bd201b6f48e8ac7859cc1fd437390000
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-27 16:05:56 +0300

    feat: integrate peer network in node

--- raft/src/node.rs
+++ raft/src/node.rs
@@ -1,16 +1,35 @@
-use crate::{Log, StateMachine};
+use crate::{Config, Log, NodeId, PeerNetwork, RaftError, StateMachine};
 
-#[derive(Default, Debug)]
 pub struct RaftNode<S: StateMachine> {
-    pub log: Log<S::Command>,
-    pub state_machine: S,
+    id: NodeId,
+    node_count: usize,
+    log: Log<S::Command>,
+    state_machine: S,
+    peers: Vec<NodeId>,
+    peer_network: PeerNetwork<S>,
 }
 
-impl<S: StateMachine> RaftNode<S> {
-    pub fn new(state_machine: S) -> Self {
+impl<S: StateMachine + 'static> RaftNode<S> {
+    pub fn new(config: &Config, state_machine: S) -> Self {
+        let peers: Vec<NodeId> = config.peers.iter().map(|p| p.id).collect();
+        let peer_network: PeerNetwork<S> = PeerNetwork::new(config.id, config.addr);
+
         RaftNode {
+            id: config.id,
+            node_count: peers.len() + 1,
             state_machine,
             log: Log::<S::Command>::default(),
+            peers,
+            peer_network,
         }
+    }
+
+    /// Starts the Raft node and listens on the supplied socket address.
+    ///
+    /// # Errors
+    ///
+    /// Returns `RaftError` if the peer networking infrastructure throws and eeror.
+    pub async fn listen(&self) -> Result<(), RaftError> {
+        self.peer_network.listen().await
     }
 }
