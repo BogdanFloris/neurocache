8793248b658243182acdf12ccfe30b4f8dd1f944
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-27 16:05:56 +0300

    feat: add client command and response to messages

--- raft/src/message.rs
+++ raft/src/message.rs
@@ -1,18 +1,36 @@
+use std::net::SocketAddr;
+
 use serde::{Deserialize, Serialize};
 
-use crate::{Entry, Index, Term};
+use crate::{Entry, Index, NodeId, StateMachine, Term};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum Message<C> {
+pub enum RaftResponse<R> {
+    Ok(R),
+    NotLeader {
+        leader_id: NodeId,
+        leader_addr: SocketAddr,
+    },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum Message<S: StateMachine> {
     AppendEntries {
         prev_log_index: Index,
         prev_log_term: Term,
-        entries: Vec<Entry<C>>,
+        entries: Vec<Entry<S::Command>>,
         leader_commit: Index,
         term: Term,
     },
     AppendEntriesResponse {
         success: bool,
         term: Term,
+    },
+    ClientCommand {
+        command: S::Command,
+    },
+    ClientResponse {
+        response: RaftResponse<S::Response>,
     },
 }
