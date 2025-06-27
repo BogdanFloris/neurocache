7b86168c42ef59774f0beb9623902be2b892bb78
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-27 08:44:21 +0300

    feat: add initial node configuration

---
+++ raft/src/config.rs
@@ -0,0 +1,33 @@
+use std::{error::Error, net::SocketAddr};
+
+use serde::{Deserialize, Serialize};
+
+pub type NodeId = u8;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Peer {
+    pub id: NodeId,
+    pub addr: SocketAddr,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    pub id: NodeId,
+    pub addr: SocketAddr,
+    pub peers: Vec<Peer>,
+}
+
+impl Config {
+    /// Loads a configuration from a JSON file
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error in two cases:
+    /// * reading the file errors
+    /// * desearializing the content of the file errors
+    pub fn from_file(path: &str) -> Result<Self, Box<dyn Error>> {
+        let file_content = std::fs::read_to_string(path)?;
+        let config: Config = serde_json::from_str(&file_content)?;
+        Ok(config)
+    }
+}
