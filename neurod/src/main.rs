696148d9580eacd867dd01a03f05a4ba93558937
Bogdan Floris <bogdan.floris@gmail.com>
2025-07-29 20:51:47 +0300

    fix: histogram buckets in grafana

--- neurod/src/main.rs
+++ neurod/src/main.rs
@@ -29,6 +29,10 @@ async fn main() -> Result<(), NeuroError> {
     tracing::subscriber::set_global_default(subscriber)?;
 
     let handle = PrometheusBuilder::new()
+        .set_buckets(&[
+            0.0001, 0.0005, 0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+        ])
+        .expect("failed to set buckets")
         .install_recorder()
         .expect("failed to install Prometheus recorder");
 
